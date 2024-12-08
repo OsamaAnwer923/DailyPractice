04562f41c5252d13053a339e4bd22a800f4a62df
OsamaAnwer923 <osama.anwer923@gmail.com>
2024-12-08 12:46:33 +0500

    Methods of List

--- December6.dart
+++ December6.dart
@@ -1,4 +1,6 @@
 void main() {
   List stutdentsRollNo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   print(stutdentsRollNo.runtimeType); // type of variable studentsRollNO.
+  int Students = stutdentsRollNo.length; // length of the list
+  print(Students); //
 }
