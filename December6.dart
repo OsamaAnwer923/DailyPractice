44467191513c2ee8c073593d4af44e7784bdfc69
OsamaAnwer923 <osama.anwer923@gmail.com>
2024-12-08 13:01:51 +0500

    Method of List

--- December6.dart
+++ December6.dart
@@ -1,6 +1,18 @@
 void main() {
   List stutdentsRollNo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  print(stutdentsRollNo.runtimeType); // type of variable studentsRollNO.
-  int Students = stutdentsRollNo.length; // length of the list
-  print(Students); //
+  // type of variable studentsRollNO.
+  print(stutdentsRollNo.runtimeType); 
+  // length of the list
+  int Students = stutdentsRollNo.length; 
+  print(Students);
+  // clear method erase the all type of data in the list
+  stutdentsRollNo.clear();
+  print(stutdentsRollNo);
+  //"add" method add elements in the list can only add one element on the last index of the list
+  stutdentsRollNo.add(1);
+  print(stutdentsRollNo);
+  // "add all" method can add multimple elments in the list
+  stutdentsRollNo.addAll([2,3,4,5,6,7,8,9]);
+  print(stutdentsRollNo);
+
 }
